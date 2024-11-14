69d439bd325793915969b4a3964b464cfe7e8f52
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-14 22:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const title = document.getElementById("title").value;
+const author = document.getElementById("author").value;
+const isbn = document.getElementById("isbn").value;
+const addBtn = document.getElementById("submit")
+
+addBtn
