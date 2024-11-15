3bf786d5e16ee6e2c8ae2e4fa1b297f92f5f346f
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-15 09:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-//your JS code here. If required.
-const title = document.getElementById("title").value;
-const author = document.getElementById("author").value;
-const isbn = document.getElementById("isbn").value;
-const addBtn = document.getElementById("submit")
+/*  your js code here. If applicable */
+const addBtn = document.getElementById("submit");
+const tbody = document.getElementById("book-list");
 
-addBtn
+addBtn.addEventListener("click", () => {
+	const title = document.getElementById("title").value;
+	const author = document.getElementById("author").value;
+	const isbn = document.getElementById("isbn").value;
+	
+	const row = document.createElement("tr")
+
+	row.innerHTML = `
+	<td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><button class="delete btn btn-danger">X</button></td>
+	`
+	
+	tbody.appendChild(row);
+
+	const delBtn = row.querySelector(".delete");
+	delBtn.addEventListener("click", () => {
+		row.remove();
+	})
+
+document.getElementById("title").value = "";
+document.getElementById("author").value = "";
+document.getElementById("isbn").value = "";
+});
